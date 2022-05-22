1f8b45f59d703d586e9f05970c8b712bf236cb29
Mark Marcello <mmarcello@nuvalence.io>
2022-05-21 22:55:32 -0400

    Added fraud db connection

---
+++ fraud/src/main/java/io/ozmani/fraud/FraudCheckHistory.java
@@ -0,0 +1,35 @@
+package io.ozmani.fraud;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FraudCheckHistory {
+
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_id_sequence"
+    )
+    private Long id;
+    private Long customerId;
+    private Boolean isFraudster;
+    private LocalDateTime createdAt;
+}
---
+++ fraud/src/main/java/io/ozmani/fraud/FraudRepository.java
@@ -0,0 +1,9 @@
+package io.ozmani.fraud;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FraudRepository extends JpaRepository<FraudCheckHistory, Long> {
+
+}
