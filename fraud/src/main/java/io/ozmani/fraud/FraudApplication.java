ae4df5a1ed7ee929e5a71db8a5238604daade885
Mark Marcello <mmarcello@nuvalence.io>
2022-05-23 23:21:48 -0400

    Set up service discovery with eureka

--- fraud/src/main/java/io/ozmani/fraud/FraudApplication.java
+++ fraud/src/main/java/io/ozmani/fraud/FraudApplication.java
@@ -2,11 +2,14 @@ package io.ozmani.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(FraudApplication.class, args);
     }
 }
