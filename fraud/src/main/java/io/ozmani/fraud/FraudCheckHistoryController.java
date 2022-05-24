91004527d7fe92fc5c02d2adac11f37dcda1a92f
Mark Marcello <mmarcello@nuvalence.io>
2022-05-23 23:37:18 -0400

    Added load balancing to customer service

--- fraud/src/main/java/io/ozmani/fraud/FraudCheckHistoryController.java
+++ fraud/src/main/java/io/ozmani/fraud/FraudCheckHistoryController.java
@@ -1,11 +1,13 @@
 package io.ozmani.fraud;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1/fraud-check/")
@@ -16,6 +18,7 @@ public class FraudCheckHistoryController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable Long customerId) {
         boolean isFraudulent = fraudCheckHistoryService.isFraudulent(customerId);
+        log.info("Fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulent);
     }
 }
