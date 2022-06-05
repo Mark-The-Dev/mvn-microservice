bb2d91e42dbbbf038a087f3946a54a8ac7f27d3c
Mark Marcello <mmarcello@nuvalence.io>
2022-06-04 22:51:11 -0400

    configured local message queue

--- notification/src/main/java/io/ozmani/notification/NotificationApplication.java
+++ notification/src/main/java/io/ozmani/notification/NotificationApplication.java
@@ -1,13 +1,36 @@
 package io.ozmani.notification;
 
+import io.ozmani.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication(
+      scanBasePackages = {
+              "io.ozmani.notification",
+              "io.ozmani.amqp"
+      }
+)
 public class NotificationApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
+
     }
+
+    // Example published message through commandline runner.
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer,
+//                                        NotificationConfig notificationConfig) {
+//        return  args -> {
+//            producer.publish(new Person("Jake", 32),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//
+//    record Person(String name, int age){}
 }
